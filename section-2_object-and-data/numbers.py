2463a77b948cdc74f1215cb2f3856628028f1e69
Jonathan <jonathan.barker09@gmail.com>
2023-08-15 16:54:45 +0000

    created some simple arithmetic with numbers

---
+++ section-2_object-and-data/numbers.py
@@ -0,0 +1,21 @@
+# You can do simple math with Python
+
+# Addition:
+x = 2
+y = 1
+print("x + y is: ",x + y)
+
+# Subtraction:
+x = 2
+y = 1
+print("x - y is: ",x - y)
+
+# Multiplication:
+x = 2
+y = 2
+print("x * y is: ",x * y)
+
+# Division:
+x = 3
+y = 2
+print("x / y is: ",x/y)
