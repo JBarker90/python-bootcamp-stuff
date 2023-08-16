23d3892a932e0d82737fcda3e4148d0457b13533
Jonathan <jonathan.barker09@gmail.com>
2023-08-16 15:55:36 +0000

    created new notes for variable assignment

---
+++ section-2_object-and-data/variable_assignment.py
@@ -0,0 +1,7 @@
+# You can assign variables and then call them later
+# Python uses Dynamic Typing, so variables can be assigned different values and still work
+
+# Simple Variable:
+
+a = 5
+print("This is the value of: ", a)
