ba403dc0d4383f3838d5c598855e1c53696712f5
Jonathan <jonathan.barker09@gmail.com>
2023-08-16 16:29:33 +0000

    added some examples of strings

---
+++ section-2_object-and-data/intro_strings.py
@@ -0,0 +1,12 @@
+# Strings are sequences of characters, using the syntax of either single quotes or double quotes:
+#   - 'hello'
+#   - "Hello"
+#   - "I don't do that"
+
+# Example of a string
+
+print("Hello World")
+
+# Another example of a string
+
+print("This is a string")
