8c5395bc7c07884d75fcb141a9ae0a0f6f8ae31e
Jonathan <jonathan.barker09@gmail.com>
2023-08-16 16:33:01 +0000

    added notes about syntax errors with strings

--- section-2_object-and-data/intro_strings.py
+++ section-2_object-and-data/intro_strings.py
@@ -10,3 +10,16 @@ print("Hello World")
 # Another example of a string
 
 print("This is a string")
+
+# You need to be careful of syntax errors like a broken string
+# This has invalid single quotes
+
+#print('I'm going on a run')
+      
+# Example:
+#
+#jonathan@ctrl-ansible:~/python_learning$ /bin/python3 /home/jonathan/python_learning/python-bootcamp-stuff/section-2_object-and-data/intro_strings.py
+#  File "/home/jonathan/python_learning/python-bootcamp-stuff/section-2_object-and-data/intro_strings.py", line 17
+#    print('I'm going on a run')
+#                             ^
+#SyntaxError: unterminated string literal (detected at line 17)
