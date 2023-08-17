9a539ea946e6ab3ce26b6daf2b6098c00e8b95d2
Jonathan <jonathan.barker09@gmail.com>
2023-08-17 15:56:45 +0000

    added notes about indexing and slicing strings

---
+++ section-2_object-and-data/indexing-slicing_strings.py
@@ -0,0 +1,13 @@
+# Slicing and Indexing with Strings
+#   - You can pass a specific character count
+#
+# Example: You can slice strings into numbers
+#
+# "H e l l o  W o r l d"
+# "0 1 2 3 4  5 6 7 8 9"
+
+mystring = "Hello World"
+print("Your string is: ", mystring)
+
+print("The first letter is ", mystring[0])
+print("The second letter is ", mystring[1])
