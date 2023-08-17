4c1927eea7c775e87d5ab2e77ed2e1619788c6ea
Jonathan <jonathan.barker09@gmail.com>
2023-08-17 16:02:16 +0000

    added another print statement

--- section-2_object-and-data/indexing-slicing_strings.py
+++ section-2_object-and-data/indexing-slicing_strings.py
@@ -3,11 +3,12 @@
 #
 # Example: You can slice strings into numbers
 #
-# "H e l l o  W o r l d"
-# "0 1 2 3 4  5 6 7 8 9"
+# "H e l l o   W o r l d"
+# "0 1 2 3 4 5 6 7 8 9 10"
 
 mystring = "Hello World"
 print("Your string is: ", mystring)
 
 print("The first letter is", mystring[0])
 print("The second letter is", mystring[1])
+print("The eighth letter is", mystring[8])
