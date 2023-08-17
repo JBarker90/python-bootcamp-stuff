aee01ede703ddcec422fe442a32b24c16b2cde7e
Jonathan <jonathan.barker09@gmail.com>
2023-08-17 16:13:17 +0000

    added negative indexing

--- section-2_object-and-data/indexing-slicing_strings.py
+++ section-2_object-and-data/indexing-slicing_strings.py
@@ -12,3 +12,16 @@ print("Your string is: ", mystring)
 print("The first letter is", mystring[0])
 print("The second letter is", mystring[1])
 print("The eighth letter is", mystring[8])
+
+# You can also use reverse indexing
+#
+# Example: This will start from the last as -1
+#
+# "  H   e  l   l   o      W  o  r  l  d"
+# "-11 -10 -9  -8  -7  -6 -5 -4 -3 -2 -1"
+
+mystring = "Hello World"
+print("Your string is: ", mystring)
+
+print("The last letter is", mystring[-1])
+print("The second to the last letter is", mystring[-2])
